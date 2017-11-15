0e32c3f7aecde560b57bf9cc08d8801cd89d87f4
Angel Buzov <angelbuzov123@gmail.com>
2017-11-15 03:05:46 +0200

    Adden index verification for "Insert" command.

--- commands/insert.js
+++ commands/insert.js
@@ -2,10 +2,13 @@ function insert(str, arr){
     let commandArgs = str.split(' ');
     
     if(commandArgs.length === 3 && commandArgs[0] === 'insert'){
+        let index = Number(commandArgs[1]);
+        let string = commandArgs[2];
+        if (index > (arr.length + 1) || index < 0) {
+            throw new Error('Error: index is out of range')
+        }
         arr.splice(commandArgs[1], 0, commandArgs[2])
     }else{
-        console.log(str)
-        console.log(arr)
         throw new Error('Error: invalid command parameters')
     }
 
